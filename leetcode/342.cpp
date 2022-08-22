8090059900920191578749fd8b68c00f8357459f
Seongsu <mscpss@naver.com>
2022-08-22 22:35:54 +0900

    leetcode : solve 342

---
+++ leetcode/342.cpp
@@ -0,0 +1,11 @@
+#include <cmath>
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        for(int i = 0; i <= 15; i++){
+            if(pow(4, i) == n) return true;
+        }
+        return false;
+    }
+};
