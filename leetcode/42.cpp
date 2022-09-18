bcefe3cdb1f7c71a9cfdefc26b9e90a910f8821a
Seongsu <mscpss@naver.com>
2022-09-18 20:08:27 +0900

    leetcode : solve 42

---
+++ leetcode/42.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int> left_max(n);
+        vector<int> right_max(n);
+        
+        left_max[0] = height[0];
+        right_max[n-1] = height[n-1];
+        
+        for(int i = 1; i < n; i++) left_max[i] = max(left_max[i-1], height[i]);
+        for(int i = n-2; i >= 0; i--) right_max[i] = max(right_max[i+1], height[i]);
+        
+        int answer = 0;
+        for(int i = 1; i < n-1; i++){
+            answer += (min(left_max[i], right_max[i]) - height[i]);
+        }
+        
+        return answer;
+    }
+};
