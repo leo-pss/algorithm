a77b0e5aae31b60f5e270ba0913ae564a0a6a89a
Seongsu <mscpss@naver.com>
2022-10-11 22:01:13 +0900

    leetcode : solve 334

---
+++ leetcode/334.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <climits>
+
+using namespace std;
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n1 = INT_MAX, n2 = INT_MAX;
+        for(int num : nums){
+            if(num <= n1) n1 = num;
+            else if(num <= n2) n2 = num;
+            else return true;
+        }
+        return false;
+    }
+};
