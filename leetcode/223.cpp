424f84e123ee44047c7464d002f02a89fd52809d
Seongsu <mscpss@naver.com>
2022-11-17 18:41:16 +0900

    leetcode : solve 223

---
+++ leetcode/223.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+        int left = max(ax1, bx1);
+        int right = min(ax2, bx2);
+        int down = max(ay1, by1);
+        int up = min(ay2, by2);
+        int iou = 0;
+        if(right > left && up > down){
+            iou = (up - down) * (right - left);
+        }
+        return area1 + area2 - iou;
+    }
+};
