0bf223cc7a10f21fe3315573e408ee80011c439e
Seongsu <mscpss@naver.com>
2022-12-26 20:51:31 +0900

    leetcode : solve 55

---
+++ leetcode/55.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int max_reach = 0;
+        for(int i = 0; i < n; i++){
+            if(i > max_reach){
+                return false;
+            }
+            max_reach = Math.max(max_reach, i + nums[i]);
+        }
+        return true;
+    }
+}
